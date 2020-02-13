e4ec58d8a11754ca0bb736d656277b14271f25ca
Giuseppe Basile <giuseppe@novoda.com>
2020-02-13 15:02:16 +0100

    List available mocks

--- web/functions/index.js
+++ web/functions/index.js
@@ -3,18 +3,25 @@ const express = require('express');
 const app = express();
 const serviceAccount = require("./firebase-admin.json");
 const admin = require('firebase-admin');
+
+const MOCK_COLLECTION = 'mocks'
+
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://mock-a50e1.firebaseio.com"
 });
 
-app.get('/mocks', (req, res) => { 
+app.get('/mocks', (req, res) => {
     admin.firestore()
-    .collection('mocks')
-    .doc('test')
-    .get()
-        .then((doc) => {
-            res.send(doc.data())
+        .collection(MOCK_COLLECTION)
+        .get()
+        .then((snapshot) => {
+            let mocks = 'List of mocks available: <br><br>'
+            snapshot.forEach(doc => {
+                mocks +=  '<a href="/mock/' + doc.id + '/">' + doc.id + '</a><br>'
+            });
+            
+            res.send(mocks)
         })
         .catch((err) => {
             res.send(err)
@@ -23,7 +30,7 @@ app.get('/mocks', (req, res) => {
 
 app.get('/mock/:path', (req, res) => {
     admin.firestore()
-        .collection('mocks')
+        .collection(MOCK_COLLECTION)
         .doc(req.params['path'])
         .get()
         .then((doc) => {
