ac645604ebffe6ceaa44d94d55f2df995a6e7829
Giuseppe Basile <giuseppe@novoda.com>
2020-02-13 13:42:59 +0100

    simple read from firestore

--- web/functions/index.js
+++ web/functions/index.js
@@ -2,6 +2,26 @@ const functions = require('firebase-functions');
 const express = require('express');
 const app = express();
 
+var serviceAccount = require("./firebase-admin.json");
+const admin = require('firebase-admin');
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://mock-a50e1.firebaseio.com"
+});
+
+app.get('/mocks', (req, res) => { 
+    admin.firestore()
+    .collection('mocks')
+    .doc('test')
+    .get()
+        .then((doc) => {
+            res.send(doc.data())
+        })
+        .catch((err) => {
+            res.send(err)
+        });
+});
+
 app.get('/hello', (req, res) => { res.send("Hello from Firebase!"); });
 
 exports.app = functions.https.onRequest(app);
