ca4ad858b498228ed73cec0f9b1fb691e670706c
Giuseppe Basile <giuseppe@novoda.com>
2020-02-13 12:50:33 +0100

    setup with express

--- web/functions/index.js
+++ web/functions/index.js
@@ -1,5 +1,7 @@
 const functions = require('firebase-functions');
+const express = require('express');
+const app = express();
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
-});
+app.get('/hello', (req, res) => { res.send("Hello from Firebase!"); });
+
+exports.app = functions.https.onRequest(app);
