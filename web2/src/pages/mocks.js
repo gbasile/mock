32c2a8fef956e5a0a3356d2b86d39391973916b0
Giuseppe Basile <giuseppe@novoda.com>
2020-02-14 16:24:23 +0100

    logout

--- web2/src/pages/mocks.js
+++ web2/src/pages/mocks.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import SignOut from '../containers/SignOut'
 
 import Layout from '../components/layout'
 import EndpointList from '../containers/EndpointList'
@@ -8,6 +9,7 @@ const SecondPage = () => (
   <Layout>
     <h1>Mocks Endpoints availables</h1>
     <EndpointList />
+    <SignOut />
   </Layout>
 )
 
